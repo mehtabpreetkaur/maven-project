a3d7acdd91b2131e7799d120a4bdf40d868902a7
mehtabpreetkaur <mehtabksidhu@gmail.com>
2025-05-14 07:16:06 +0000

    Initial commit

---
+++ src/test/java/com/example/DemoTest.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class DemoTest {
+    
+@Test Program Manager - AHM, SAFe Agilist
+
+    public void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
